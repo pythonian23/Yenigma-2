c90935bbd4f213bc51217d002cdde7dca01434f4
pythonian23 <63051526+pythonian23@users.noreply.github.com>
2020-08-12 18:35:27 +0900

    added rotor creation and output

--- Yenigma/yenigma.py
+++ Yenigma/yenigma.py
@@ -6,12 +6,27 @@ class Yenigma:
     def set_base(self, number_of_rings, ring_set="abcdefghijklmnopqrstuvwxyz"):
         self.ring_chars = ring_set
         self.ring_count = number_of_rings
-        self.ring_set = list(self.ring_chars) * self.ring_count
+        self.rotors = [list(self.ring_chars)] * self.ring_count
+        self.rotations = [0]*self.ring_count
         self.reflector = dict(zip(self.ring_chars, self.ring_chars))
+        self.full_set = [self.rotors, self.reflector]
 
-        return self.ring_set
+        return self.full_set
+
+    def rotor_f(self, char, ring):
+        if (loc := self.ring_chars.find(char)) == -1:
+            return ""
+        else:
+            return self.rotors[ring][loc]
+
+    def rotor_b(self, char, ring):
+        if char in self.ring_chars:
+            return self.rotors[ring][self.ring_chars.index(char)]
+        else:
+            return ""
 
 
 if __name__ == '__main__':
     yenigma = Yenigma()
-    print(yenigma.reflector)
+
+    print(yenigma.rotor_b("a", 1))
